a4d69724beeeb5c2f138539f17c68296814b9457
zyltntking <zyltntking@live.cn>
2023-04-13 13:39:01 +0800

    创建测试和代码片段项目

---
+++ Artemis.Test.Core/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Artemis.Test.Core
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ test/Artemis.Test.ConsoleSnippets/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Artemis.Test.ConsoleSnippets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bb = File.ReadAllLines("/etc/os-release");
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
