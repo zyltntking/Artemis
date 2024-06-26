71974aeab04d160433df41de8f90c2407bbe20ed
Silmeria <zyltntking@qq.com>
2024-06-26 13:35:45 +0800

    添加解决方案筛选器，添加包筛选

---
+++ src/Services/Artemis.Service.Protos/ProtobufExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Artemis.Service.Protos;
+
+/// <summary>
+/// 协议缓冲扩展
+/// </summary>
+public static class ProtobufExtensions
+{
+    /// <summary>
+    /// 后缀名
+    /// </summary>
+    public const string ProtobufFileExtension = ".proto";
+}
