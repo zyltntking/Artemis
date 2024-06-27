94677b591c14b5f7cdfbcaf18cda213eac96db61
Silmeria <zyltntking@qq.com>
2024-06-27 18:13:12 +0800

    删除外部文件

--- src/Services/Artemis.Service.Protos/ProtobufExtensions.cs
+++ src/Services/Artemis.Service.Protos/ProtobufExtensions.cs
@@ -1,12 +1,17 @@
 ﻿namespace Artemis.Service.Protos;
 
 /// <summary>
-/// 协议缓冲扩展
+///     协议缓冲扩展
 /// </summary>
 public static class ProtobufExtensions
 {
     /// <summary>
-    /// 后缀名
+    ///     后缀名
     /// </summary>
     public const string ProtobufFileExtension = ".proto";
+
+    /// <summary>
+    ///     版本号
+    /// </summary>
+    public const string Version = "v1";
 }
