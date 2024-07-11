84bf79547afd5e43eef97278a48d5147b0838a93
Silmeria <zyltntking@qq.com>
2024-07-11 23:26:03 +0800

    删除非必要项目

--- src/Data/Artemis.Data.Shared/RawData/IOptoMeter.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Artemis.Data.Shared.RawData;
-
-/// <summary>
-///     验光仪数据接口
-/// </summary>
-public interface IOptometer
-{
-}
--- src/Data/Artemis.Data.Shared/School/IClassRoom.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Artemis.Data.Shared.School;
-
-/// <summary>
-///     教室接口
-/// </summary>
-public interface IClassRoom
-{
-}
